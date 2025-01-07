351f85db50fbf5c6a91f55dc4d47153948cc17cc
natejank <njj3397@rit.edu>
2025-01-06 22:19:53 -0500

    initial commit

---
+++ src/main/java/org/team2168/HolidayPotluck/Dinner.java
@@ -0,0 +1,8 @@
+package org.team2168.HolidayPotluck;
+
+public class Dinner {
+    /* This is the main file in your program!
+    All you really need to do here is give java a starting place to actually run your code,
+    instantiate some food and attendees, and have them eat and converse! */
+
+}
---
+++ src/main/java/org/team2168/HolidayPotluck/FoodItems/Peas.java
@@ -0,0 +1,17 @@
+package org.team2168.HolidayPotluck.FoodItems;
+
+public class Peas extends Food {
+    /* Member Variables:
+      Think of what attributes peas specifically would have.
+        - peas are typically eaten hot, so this is something to keep track of
+        - Get creative! */
+
+    /* Constructors: (overload me!) */
+
+    /* Methods:
+      - Setters and getters for your peas-specific variables
+      - Change the way tastiness is done.  No one likes cold peas, so the tastiness should reflect that.
+        - you will need to `@Override` one of the constructors in Food to accomplish that.
+    */
+
+}
